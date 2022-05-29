1582f3589f88668eb75d565e2b1aa0a1b937f601
Daniel Apodaca <daniela@zoomcatalog.com>
2022-05-28 18:56:08 -0600

    navbar changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,48 @@
+import { Menu, Transition } from '@headlessui/react'
 import Link from 'next/link'
 import { useContext } from 'react'
 import { UserContext } from '../lib/context'
+import { useAuthentication } from '../lib/useAuthentication'
 import styles from '../styles/Navbar.module.scss'
+import { DMenu, DMenuButton, DPopover } from './primitives'
 
 const Navbar: React.FC = () => {
   const { user } = useContext(UserContext)
+  const authInstance = useAuthentication()
+  const profileOptions = [
+    {
+      content: 'Profile',
+      action: () => console.log('Profile'),
+    },
+    {
+      content: 'Log out',
+      action: () => authInstance.signOut(),
+    },
+  ]
 
-  return <nav className={styles.navbar}>
-    <div className={styles['navbar__logo']}>
-      <Link href="/">UltraColors</Link></div>
-    <div className={styles['navbar__actions']}>
-      <div className="navbar__units">Hex</div>
-      <div className="navbar__language">English</div>
-      <div className="navbar__account">
-        {user && <Link href="/profile/:userId">Profile</Link>}
-        {!user && <Link href="/sign-in">Sign in</Link>}
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles['navbar__logo']}>
+        <Link href="/">UltraColors</Link>
       </div>
-    </div>
-  </nav>
+      <div className={styles['navbar__actions']}>
+        <div className="navbar__units">Hex</div>
+        <div className="navbar__language">English</div>
+        <div className="navbar__account">
+          {user && (
+            <DMenu options={profileOptions}>
+              <DMenuButton>{user.displayName}</DMenuButton>
+            </DMenu>
+          )}
+          {!user && (
+            <Link href="/sign-in">
+              <DPopover />
+            </Link>
+          )}
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
