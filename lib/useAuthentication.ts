85a5772deaf65a726394f46f39f27c5e0c0cba9a
Daniel Apodaca <daniela@zoomcatalog.com>
2022-05-30 07:57:02 -0600

    call cloud function when user signs up

--- lib/useAuthentication.ts
+++ lib/useAuthentication.ts
@@ -1,15 +1,21 @@
 import { signInWithPopup, signOut as signOutFirebase } from "firebase/auth"
+import { httpsCallable } from "firebase/functions"
 import { useAuthState } from "react-firebase-hooks/auth"
 import { errorHandler } from "./errorHandler"
-import { auth, googleAuthProvider } from "./firebase"
+import { auth, functions, googleAuthProvider } from "./firebase"
 
 export const useAuthentication = () => {
+  // State
   const [user] = useAuthState(auth)
+
+  // Callable functions
+  const onSignUp = httpsCallable(functions, 'onSignUp')
 
   // Handlers
   async function signInWithGoogle() {
     try {
       await signInWithPopup(auth, googleAuthProvider)
+      await onSignUp()
     } catch (err) {
       errorHandler(err)
     }
